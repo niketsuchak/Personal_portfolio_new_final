9b546ea7f1f24c192a2f0150a54ee0f7dd168088
niketsuchak <niket.suchak2021@vitstudent.ac.in>
2024-06-13 22:22:47 +0530

    final

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/niketsuchak/Newsify"
+          h3="Newsify"
+          p="News App"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Currency Converter"
+          p="Converts Currency"
+        />
+        {/* <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="FitLift"
+          p="Fitness App"
+        /> */}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
